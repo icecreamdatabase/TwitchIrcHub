fc0c52f54970ad2d8038abdecbbb579fd154a6b0
Karl <git@karlneugebauer.com>
2021-10-10 19:49:02 +0200

    reordered and group tags better

--- TwitchIrcHub/IrcBot/Irc/DataTypes/Parsed/IrcPrivMsg.cs
+++ TwitchIrcHub/IrcBot/Irc/DataTypes/Parsed/IrcPrivMsg.cs
@@ -20,11 +20,15 @@ public class IrcPrivMsg
     public Dictionary<string, string[]> Flags { get; }
     public string Id { get; }
     public int RoomId { get; }
-    public string RoomName { get; }
     public DateTime TmiSentTs { get; }
     public int UserId { get; }
-    public string UserName { get; }
+
+    /* --------------------------------------------------------------------------- */
+    /* --------------------- Non-tag but still required data --------------------- */
+    /* --------------------------------------------------------------------------- */
     public string Message { get; }
+    public string RoomName { get; }
+    public string UserName { get; }
 
     /* --------------------------------------------------------------------------- */
     /* ----------------------------- Conditional tags ---------------------------- */
@@ -34,6 +38,7 @@ public class IrcPrivMsg
     public string? ClientNonce { get; }
     public string? CrowdChantParentMsgId { get; }
     public string? CustomRewardId { get; }
+    public bool? EmoteOnly { get; }
     public string? ReplyParentMsgId { get; }
     public string? ReplyParentUserId { get; }
     public string? ReplyParentUserLogin { get; }
@@ -47,26 +52,20 @@ public class IrcPrivMsg
 
         Raw = ircMessage;
 
+
         // Try parsing all known tags.
+        /* --------------------------------------------------------------------------- */
+        /* ------------------------------ Required tags ------------------------------ */
+        /* --------------------------------------------------------------------------- */
         ircMessage.IrcMessageTags.TryGetValue("badge-info", out string? badgeInfo);
         ircMessage.IrcMessageTags.TryGetValue("badges", out string? badges);
-        ircMessage.IrcMessageTags.TryGetValue("bits", out string? bits);
-        ircMessage.IrcMessageTags.TryGetValue("bits-img-url", out string? bitsImageUrl);
-        ircMessage.IrcMessageTags.TryGetValue("client-nonce", out string? clientNonce);
         ircMessage.IrcMessageTags.TryGetValue("color", out string? color);
-        ircMessage.IrcMessageTags.TryGetValue("crowd-chant-parent-msg-id", out string? crowdChantParentMsgId);
-        ircMessage.IrcMessageTags.TryGetValue("custom-reward-id", out string? customRewardId);
         ircMessage.IrcMessageTags.TryGetValue("display-name", out string? displayName);
         ircMessage.IrcMessageTags.TryGetValue("emotes", out string? emotes);
         ircMessage.IrcMessageTags.TryGetValue("first-msg", out string? firstMsg);
         ircMessage.IrcMessageTags.TryGetValue("flags", out string? flags);
         ircMessage.IrcMessageTags.TryGetValue("id", out string? id);
         //ircMessage.IrcMessageTags.TryGetValue("mod", out string? mod);
-        ircMessage.IrcMessageTags.TryGetValue("reply-parent-msg-id", out string? replyParentMsgId);
-        ircMessage.IrcMessageTags.TryGetValue("reply-parent-user-id", out string? replyParentUserId);
-        ircMessage.IrcMessageTags.TryGetValue("reply-parent-user-login", out string? replyParentUserLogin);
-        ircMessage.IrcMessageTags.TryGetValue("reply-parent-display-name", out string? replyParentDisplayName);
-        ircMessage.IrcMessageTags.TryGetValue("reply-parent-msg-body", out string? replyParentMsgBody);
         ircMessage.IrcMessageTags.TryGetValue("room-id", out string? roomId);
         //ircMessage.IrcMessageTags.TryGetValue("subscriber", out string? subscriber);
         ircMessage.IrcMessageTags.TryGetValue("tmi-sent-ts", out string? tmiSentTs);
@@ -74,7 +73,22 @@ public class IrcPrivMsg
         ircMessage.IrcMessageTags.TryGetValue("user-id", out string? userId);
         //ircMessage.IrcMessageTags.TryGetValue("user-type", out string? userType);
 
-        // Exceptions for missing tags that are not allowed to be missing.
+        /* --------------------------------------------------------------------------- */
+        /* ----------------------------- Conditional tags ---------------------------- */
+        /* --------------------------------------------------------------------------- */
+        ircMessage.IrcMessageTags.TryGetValue("bits", out string? bits);
+        ircMessage.IrcMessageTags.TryGetValue("bits-img-url", out string? bitsImageUrl);
+        ircMessage.IrcMessageTags.TryGetValue("client-nonce", out string? clientNonce);
+        ircMessage.IrcMessageTags.TryGetValue("crowd-chant-parent-msg-id", out string? crowdChantParentMsgId);
+        ircMessage.IrcMessageTags.TryGetValue("custom-reward-id", out string? customRewardId);
+        ircMessage.IrcMessageTags.TryGetValue("emote-only", out string? emoteOnly);
+        ircMessage.IrcMessageTags.TryGetValue("reply-parent-msg-id", out string? replyParentMsgId);
+        ircMessage.IrcMessageTags.TryGetValue("reply-parent-user-id", out string? replyParentUserId);
+        ircMessage.IrcMessageTags.TryGetValue("reply-parent-user-login", out string? replyParentUserLogin);
+        ircMessage.IrcMessageTags.TryGetValue("reply-parent-display-name", out string? replyParentDisplayName);
+        ircMessage.IrcMessageTags.TryGetValue("reply-parent-msg-body", out string? replyParentMsgBody);
+
+        // Exceptions for nullable / missing tags that are not allowed to be missing.
         if (string.IsNullOrEmpty(id))
             throw new Exception($"PRIVMSG without valid id:\n{ircMessage.RawSource}");
         if (string.IsNullOrEmpty(roomId))
@@ -91,31 +105,42 @@ public class IrcPrivMsg
             throw new Exception($"PRIVMSG without valid timestamp:\n{ircMessage.RawSource}");
 
         // Assign parsed tags to properties.
+        /* --------------------------------------------------------------------------- */
+        /* ------------------------------ Required tags ------------------------------ */
+        /* --------------------------------------------------------------------------- */
         BadgeInfo = ParseBadgeData(badgeInfo);
         Badges = ParseBadgeData(badges);
-        Bits = bits;
-        BitsImgUrl = bitsImageUrl;
-        ClientNonce = clientNonce;
         Color = color ?? "";
-        CrowdChantParentMsgId = crowdChantParentMsgId;
-        CustomRewardId = customRewardId;
         DisplayName = displayName;
         Emotes = ParseEmoteData(emotes);
         FirstMsg = firstMsg != "0";
         Flags = ParseFlags(flags);
         Id = id;
+        RoomId = int.Parse(roomId);
+        TmiSentTs = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(tmiSentTs)).UtcDateTime;
+        UserId = int.Parse(userId);
+
+        /* --------------------------------------------------------------------------- */
+        /* --------------------- Non-tag but still required data --------------------- */
+        /* --------------------------------------------------------------------------- */
+        Message = ircMessage.IrcParameters[1];
+        RoomName = ircMessage.IrcParameters[0][1..];
+        UserName = ircMessage.IrcPrefix.Username;
+
+        /* --------------------------------------------------------------------------- */
+        /* ----------------------------- Conditional tags ---------------------------- */
+        /* --------------------------------------------------------------------------- */
+        Bits = bits;
+        BitsImgUrl = bitsImageUrl;
+        ClientNonce = clientNonce;
+        CrowdChantParentMsgId = crowdChantParentMsgId;
+        CustomRewardId = customRewardId;
+        EmoteOnly = emoteOnly != "0";
         ReplyParentMsgId = replyParentMsgId;
         ReplyParentUserId = replyParentUserId;
         ReplyParentUserLogin = replyParentUserLogin;
         ReplyParentDisplayName = replyParentDisplayName;
         ReplyParentMsgBody = replyParentMsgBody;
-        RoomId = int.Parse(roomId);
-        TmiSentTs = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(tmiSentTs)).UtcDateTime;
-        UserId = int.Parse(userId);
-
-        RoomName = ircMessage.IrcParameters[0][1..];
-        Message = ircMessage.IrcParameters[1];
-        UserName = ircMessage.IrcPrefix.Username;
     }
 
     private static Dictionary<string, int> ParseBadgeData(string? value)
