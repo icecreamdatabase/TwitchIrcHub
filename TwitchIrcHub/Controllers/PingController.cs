11565cef708b69c63307858e308e1c9cec13e339
Karl <git@karlneugebauer.com>
2022-02-02 20:59:22 +0100

    Added PingController; cleanup

---
+++ TwitchIrcHub/Controllers/PingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TwitchIrcHub.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PingController : ControllerBase
+{
+    private readonly ILogger<TestController> _logger;
+
+    public PingController(ILogger<TestController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Get()
+    {
+        return Ok("Pong");
+    }
+}
